386c97a6f0f8239dc3583f57570bd75096f7755d
Roberto Huertas <roberto.huertas@outlook.com>
2025-02-07 02:31:44 +0100

    support for multiple rgb

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use clap::Parser;
 use image::{ImageBuffer, Rgb};
 use resvg::{self, usvg};
-use std::path::PathBuf;
+use std::{path::PathBuf, vec};
 use tiny_skia::{Pixmap, PremultipliedColorU8};
 use usvg::Transform;
 
@@ -17,6 +17,9 @@ struct Cli {
     /// Width of the output icon
     #[arg(short = 'w', long, default_value = "200")]
     icon_width: u32,
+    /// Additional background colors in RGB format (e.g., 255,0,0)
+    #[arg(short, long, value_delimiter = ' ', num_args = 1..)]
+    backgrounds: Vec<String>,
 }
 
 fn main() -> Result<()> {
@@ -43,23 +46,39 @@ fn main() -> Result<()> {
     // Render SVG with scaling
     resvg::render(&tree, transform, &mut pixmap.as_mut());
 
-    // Create image (dark + light)
-    let mut combined = ImageBuffer::<Rgb<u8>, _>::new(icon_width * 3, icon_width);
+    let mut bg_colors = vec![PremultipliedColorU8::from_rgba(255, 255, 255, 255)];
 
-    let white_background = PremultipliedColorU8::from_rgba(255, 255, 255, 255);
-    let beige_background = PremultipliedColorU8::from_rgba(245, 245, 220, 255);
+    for bg in cli.backgrounds {
+        let rgb: Vec<u8> = bg
+            .split(',')
+            .map(|c| c.parse().expect("Invalid color format"))
+            .collect();
+
+        if rgb.len() != 3 {
+            panic!("Invalid RGB format: {}", bg);
+        }
+
+        bg_colors.push(PremultipliedColorU8::from_rgba(rgb[0], rgb[1], rgb[2], 255));
+    }
+
+    // Create image with the width of all the backgrounds
+    let mut combined =
+        ImageBuffer::<Rgb<u8>, _>::new(icon_width * (bg_colors.len() as u32 + 1), icon_width);
 
     for y in 0..icon_width {
         for x in 0..icon_width {
             if let Some(pixel) = pixmap.pixel(x, y) {
                 // dark background (original)
-                combined.put_pixel(x, y, blend(pixel, None));
+                combined.put_pixel(x, y, blend(&pixel, None));
 
-                // white background
-                combined.put_pixel(x + icon_width, y, blend(pixel, white_background));
-
-                // beige background
-                combined.put_pixel(x + icon_width * 2, y, blend(pixel, beige_background));
+                // additional backgrounds
+                for (i, background) in bg_colors.iter().enumerate() {
+                    combined.put_pixel(
+                        x + icon_width * (i as u32 + 1),
+                        y,
+                        blend(&pixel, *background),
+                    );
+                }
             }
         }
     }
@@ -70,7 +89,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn blend(pixel: PremultipliedColorU8, background: Option<PremultipliedColorU8>) -> Rgb<u8> {
+fn blend(pixel: &PremultipliedColorU8, background: Option<PremultipliedColorU8>) -> Rgb<u8> {
     if let Some(background) = background {
         let a = pixel.alpha() as f32 / 255.0;
         let blend_color = |c: u8, bg: u8| ((c as f32 * a) + (bg as f32 * (1.0 - a))) as u8;
