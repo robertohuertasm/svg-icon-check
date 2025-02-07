8bcfb25b51b81c2757e57f32c2ad7510a55a2059
Roberto Huertas <roberto.huertas@outlook.com>
2025-02-07 01:10:55 +0100

    v0

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use anyhow::Result;
+use clap::Parser;
+use image::{ImageBuffer, Rgb};
+use resvg::{self, usvg};
+use tiny_skia::{Pixmap, PremultipliedColorU8};
+use usvg::Transform;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+    /// Path to the input SVG file
+    input: PathBuf,
+    /// Path to the output PNG file
+    output: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    // Loading and parsing the SVG file
+    let svg_data = std::fs::read(&cli.input)?;
+    let opt = usvg::Options::default();
+    let tree = usvg::Tree::from_data(&svg_data, &opt)?;
+
+    // Get SVG dimensions
+    let (width, height) = (tree.size().width() as u32, tree.size().height() as u32);
+
+    // Create Pixmap
+    let mut pixmap = Pixmap::new(width, height)
+        .ok_or_else(|| anyhow::anyhow!("Couldn't create the Pixmap"))?;
+
+    // Render SVG
+    resvg::render(
+        &tree,
+        Transform::identity(),
+        &mut pixmap.as_mut(), 
+    );
+
+    // Create image (dark + light)
+    let mut combined = ImageBuffer::<Rgb<u8>, _>::new(width * 2, height);
+
+    let white_background = PremultipliedColorU8::from_rgba(255, 255, 255, 255);
+
+    for y in 0..height {
+        for x in 0..width {
+            if let Some(pixel) = pixmap.pixel(x, y) {
+                // dark background (original)
+                combined.put_pixel(x, y, blend(pixel, None));
+
+                // white background
+                combined.put_pixel(
+                    x + width,
+                    y,
+                    blend(pixel, white_background)
+                );
+            }
+        }
+    }
+
+    // saving the PNG
+    combined.save(cli.output)?;
+
+    Ok(())
+}
+
+fn blend(pixel: PremultipliedColorU8, background: Option<PremultipliedColorU8>) -> Rgb<u8> {
+    if let Some(background) = background {
+        let a = pixel.alpha() as f32 / 255.0;
+        let blend_color = |c: u8, bg: u8| ((c as f32 * a) + (bg as f32 * (1.0 - a))) as u8;
+        Rgb([
+            blend_color(pixel.red(), background.red()),
+            blend_color(pixel.green(), background.green()),
+            blend_color(pixel.blue(), background.blue()),
+        ])
+    } else {
+        Rgb([pixel.red(), pixel.green(), pixel.blue()])
+    }
+   
+}
